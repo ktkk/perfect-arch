ea23e7ec94e3ca847c6c7fd7a2d0ce3cf7f5c831
ktkk <dezuttereluka@gmail.com>
2021-07-25 17:36:07 +0200

    Added some more config options

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,5 +2,11 @@ local wezterm = require("wezterm")
 local theme = require("theme")
 
 return {
-	colors = theme.colors
+	colors = theme.colors,
+
+	front_end = "OpenGL",
+	enable_wayland = false,
+
+	check_for_updates = false,
+	window_close_confirmation = "NeverPrompt",
 }
