097c33b05d47389bd3b276815af3fb1dab65112e
ktkk <dezuttereluka@gmail.com>
2021-11-13 13:49:11 +0100

    Fixed typos

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -10,14 +10,13 @@ end
 -- Normal mode mappings
 map('n', "<C-t>", ":term<CR>") -- Open term with C-t
 
-map('n', "M-j", "m .+1<CR>==") -- Visual Studio style line moving
-map('n', "M-k", "m .-1<CR>==")
+map('n', "<M-j>", ":m .+1<CR>==") -- Visual Studio style line moving
+map('n', "<M-k>", ":m .-2<CR>==")
 
 map('n', "<C-S-b>", ":make<CR>") -- Invoke make with C-S-b
 
 -- Visual mode mappings
 map('v', "<M-j>", ":m '>+1<CR>gv=gv") -- Visual Studio style line moving
-map('v', "<M-j>", ":m '>-2<CR>gv=gv")
+map('v', "<M-k>", ":m '>-2<CR>gv=gv")
 
 map('v', "<C-c>", "\"*y :let @+=@*<CR>")
-
